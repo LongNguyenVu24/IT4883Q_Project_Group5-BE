a12b396b85b979af2a4ba0160550cafbd318712d
Nguyen Vu Long <86005133+LongNguyenVu24@users.noreply.github.com>
2023-05-04 08:34:06 +0700

    sub

---
+++ taskcrud/taskcrud/src/main/java/com/example/taskcrud/entity/subTask.java
@@ -0,0 +1,64 @@
+package com.example.taskcrud.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "subTask")
+public class subTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subtask_Id", length  = 50)
+    private int subtaskId;
+    @Column(name = "subTask_name", length = 255)
+    private String subTaskName;
+
+    @Column(name = "step", length = 255)
+    private String step;
+
+    @Column(name = "status")
+    private boolean status;
+
+    public subTask(int subtaskId, String subTaskName, String step, boolean status) {
+        this.subtaskId = subtaskId;
+        this.subTaskName = subTaskName;
+        this.step = step;
+        this.status = status;
+    }
+
+    public subTask() {
+    }
+
+    public int getSubtaskId() {
+        return subtaskId;
+    }
+
+    public void setSubtaskId(int subtaskId) {
+        this.subtaskId = subtaskId;
+    }
+
+    public String getSubTaskName() {
+        return subTaskName;
+    }
+
+    public void setSubTaskName(String subTaskName) {
+        this.subTaskName = subTaskName;
+    }
+
+    public String getStep() {
+        return step;
+    }
+
+    public void setStep(String step) {
+        this.step = step;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
+
+
