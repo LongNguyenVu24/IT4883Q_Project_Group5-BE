fb6720babb10a8a4f32d6056f7ba6cf28abab47b
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-05-10 09:37:33 +0700

    finish CRUD for Task

--- taskcrud/taskcrud/src/main/java/com/example/taskcrud/Controller/TaskController.java
+++ taskcrud/taskcrud/src/main/java/com/example/taskcrud/Controller/TaskController.java
@@ -1,4 +1,45 @@
 package com.example.taskcrud.Controller;
 
+
+import com.example.taskcrud.DTO.TaskDTO;
+import com.example.taskcrud.DTO.TaskSaveDTO;
+import com.example.taskcrud.DTO.TaskUpdateDTO;
+import com.example.taskcrud.Impl.RecordServiceImpl;
+import com.example.taskcrud.Impl.TaskServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/task")
 public class TaskController {
+    @Autowired
+    private TaskServiceImpl taskService;
+
+    @PostMapping(path = "/save")
+    public String saveTask(@RequestBody TaskSaveDTO taskSaveDTO) {
+
+        String taskID = taskService.addTask(taskSaveDTO);
+        return taskID;
+    }
+
+    @GetMapping(path = "/getAllTasks")
+    public List<TaskDTO> getAllTask() {
+        List<TaskDTO>allTasks = taskService.getAllTask();
+        return allTasks;
+    }
+
+    @GetMapping(path = "/update")
+    public String updateTask(@RequestBody TaskUpdateDTO taskUpdateDTO) {
+        String taskId = taskService.updateTask(taskUpdateDTO);
+        return taskId;
+    }
+
+    @DeleteMapping(path = "/deletetask/{taskId}")
+    public String deleteTask(@PathVariable(value = "taskId") int taskId) {
+        boolean deleteTask = taskService.deleteTask(taskId);
+        return "deleted";
+    }
 }
