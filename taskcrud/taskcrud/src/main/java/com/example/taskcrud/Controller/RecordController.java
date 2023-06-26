4e092025e218c84eaa55eea4e7586bd54a3511fc
LeThanhDat <89300362+lethanhdathust@users.noreply.github.com>
2023-06-26 09:54:18 +0700

    fix bug

--- taskcrud/taskcrud/src/main/java/com/example/taskcrud/Controller/RecordController.java
+++ taskcrud/taskcrud/src/main/java/com/example/taskcrud/Controller/RecordController.java
@@ -1,80 +1,80 @@
-package com.example.taskcrud.Controller;
-
-
-import com.example.taskcrud.Impl.RecordService;
-import com.example.taskcrud.Repository.RecordingRepository;
-import com.example.taskcrud.entity.Recordings;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Optional;
-
-@RestController
-@RequestMapping("/recordings")
-@Slf4j
-public class RecordController {
-
-    @Autowired
-    private RecordService recordService;
-
-    @GetMapping("/testData")
-    public String testData(HttpServletRequest request){
-        log.info("[testData] da truy cap thanh cong {}", request.getHeader("thuoctinha"));
-
-        return "";
-    }
-
-
-    @PostMapping("/createRecording")
-    public ResponseEntity<String> createRecording(@RequestParam("file") MultipartFile file,
-                                                  @RequestParam("fileDir") String fileDir) {
-        try {
-            Recordings createdRecording = recordService.createRecording(file, fileDir);
-            return ResponseEntity.ok("Recording created successfully. ID: " + createdRecording.getId());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create recording: " + e.getMessage());
-        }
-    }
-
-    @GetMapping("/{id}/play")
-    public ResponseEntity<byte[]> playRecording(@PathVariable("id") String id) {
-        Optional<Recordings> optionalRecordings = recordService.getRecordingById(String.valueOf(id));
-        if (optionalRecordings.isPresent()) {
-            Recordings recordings = optionalRecordings.get();
-            try {
-                byte[] bytes = recordService.getRecordingBytes(recordings);
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-                headers.setContentDisposition(ContentDisposition.builder("attachment").filename(recordings.getName()).build());
-                return  new ResponseEntity<>(bytes, headers, HttpStatus.OK);
-            } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-            }
-        }
-        else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @DeleteMapping("{id}/delete")
-    public ResponseEntity<?> deleteRecording(@PathVariable("id") String id) {
-        try {
-            recordService.deleteRecordingById(String.valueOf(id));
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-}
+//package com.example.taskcrud.Controller;
+//
+//
+//import com.example.taskcrud.Impl.RecordService;
+//import com.example.taskcrud.Repository.RecordingRepository;
+//import com.example.taskcrud.entity.Recordings;
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.*;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.sound.sampled.AudioFormat;
+//import javax.sound.sampled.AudioInputStream;
+//import javax.sound.sampled.AudioSystem;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.StandardCopyOption;
+//import java.util.Optional;
+//
+//@RestController
+//@RequestMapping("/recordings")
+//@Slf4j
+//public class RecordController {
+//
+//    @Autowired
+//    private RecordService recordService;
+//
+//    @GetMapping("/testData")
+//    public String testData(HttpServletRequest request){
+//        log.info("[testData] da truy cap thanh cong {}", request.getHeader("thuoctinha"));
+//
+//        return "";
+//    }
+//
+//
+//    @PostMapping("/createRecording")
+//    public ResponseEntity<String> createRecording(@RequestParam("file") MultipartFile file,
+//                                                  @RequestParam("fileDir") String fileDir) {
+//        try {
+//            Recordings createdRecording = recordService.createRecording(file, fileDir);
+//            return ResponseEntity.ok("Recording created successfully. ID: " + createdRecording.getId());
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create recording: " + e.getMessage());
+//        }
+//    }
+//
+//    @GetMapping("/{id}/play")
+//    public ResponseEntity<byte[]> playRecording(@PathVariable("id") String id) {
+//        Optional<Recordings> optionalRecordings = recordService.getRecordingById(String.valueOf(id));
+//        if (optionalRecordings.isPresent()) {
+//            Recordings recordings = optionalRecordings.get();
+//            try {
+//                byte[] bytes = recordService.getRecordingBytes(recordings);
+//                HttpHeaders headers = new HttpHeaders();
+//                headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+//                headers.setContentDisposition(ContentDisposition.builder("attachment").filename(recordings.getName()).build());
+//                return  new ResponseEntity<>(bytes, headers, HttpStatus.OK);
+//            } catch (Exception e) {
+//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//            }
+//        }
+//        else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+//
+//    @DeleteMapping("{id}/delete")
+//    public ResponseEntity<?> deleteRecording(@PathVariable("id") String id) {
+//        try {
+//            recordService.deleteRecordingById(String.valueOf(id));
+//            return ResponseEntity.ok().build();
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//}
--- taskcrud/taskcrud/src/main/java/com/example/taskcrud/Impl/RecordService.java
+++ taskcrud/taskcrud/src/main/java/com/example/taskcrud/Impl/RecordService.java
@@ -1,102 +1,102 @@
-package com.example.taskcrud.Impl;
-
-import com.example.taskcrud.Repository.RecordingRepository;
-import com.example.taskcrud.entity.Recordings;
-import org.apache.tika.Tika;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.sound.sampled.*;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.util.Optional;
-
-@Service
-public class RecordService {
-    @Autowired
-    private RecordingRepository recordingRepository;
-    @Value("${fileDir}")
-    private String fileDir;
-
-    public Recordings createRecording(MultipartFile file, String fileDir) throws Exception {
-        AudioFormat audioFormat = new AudioFormat(44100, 16, 2, true, false);
-        DataLine.Info targetDataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
-
-        if (!AudioSystem.isLineSupported(targetDataLineInfo)) {
-            throw new LineUnavailableException("Microphone is not supported.");
-        }
-
-        TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(targetDataLineInfo);
-        targetDataLine.open(audioFormat);
-        targetDataLine.start();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-
-        int bufferSize = 4096;
-        byte[] buffer = new byte[bufferSize];
-
-        long startTime = System.currentTimeMillis();
-
-        System.out.println("Recording started.");
-
-        while (System.currentTimeMillis() - startTime < 5000) { // Adjust the duration of recording as needed
-            int bytesRead = targetDataLine.read(buffer, 0, buffer.length);
-            byteArrayOutputStream.write(buffer, 0, bytesRead);
-        }
-
-        System.out.println("Recording finished.");
-
-        targetDataLine.stop();
-        targetDataLine.close();
-
-        byte[] audioData = byteArrayOutputStream.toByteArray();
-
-
-
-
-
-        //Create Directory
-        File directory = new File(fileDir);
-        if (!directory.exists()) {
-            boolean created = directory.mkdirs();
-            if (!created) {
-                throw new IllegalStateException("Failed to create the directory: " + fileDir);
-            }
-        }
-        String name = file.getOriginalFilename();
-
-        Recordings recordings = new Recordings();
-
-        recordings.setName(name);
-        recordings.setFormat(audioFormat.toString());
-        recordings.setDuration(5000L);
-        recordings.setBytes(audioData);
-        recordings.setFilePath(fileDir + File.separator + name);
-
-
-
-        recordingRepository.save(recordings);
-        return recordings;
-
-
-    }
-
-    public Optional<Recordings> getRecordingById(String id) {
-        return recordingRepository.findById(id);
-    }
-
-    public void deleteRecordingById( String id) {
-        recordingRepository.deleteById(id);
-
-    }
-
-    public byte[] getRecordingBytes(Recordings recordings) throws  Exception {
-        return  recordings.getBytes();
-    }
-
-
-}
+//package com.example.taskcrud.Impl;
+//
+//import com.example.taskcrud.Repository.RecordingRepository;
+//import com.example.taskcrud.entity.Recordings;
+//import org.apache.tika.Tika;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.sound.sampled.*;
+//import java.io.BufferedInputStream;
+//import java.io.ByteArrayOutputStream;
+//import java.io.File;
+//import java.io.InputStream;
+//import java.util.Optional;
+//
+//@Service
+//public class RecordService {
+//    @Autowired
+//    private RecordingRepository recordingRepository;
+//    @Value("${fileDir}")
+//    private String fileDir;
+//
+//    public Recordings createRecording(MultipartFile file, String fileDir) throws Exception {
+//        AudioFormat audioFormat = new AudioFormat(44100, 16, 2, true, false);
+//        DataLine.Info targetDataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
+//
+//        if (!AudioSystem.isLineSupported(targetDataLineInfo)) {
+//            throw new LineUnavailableException("Microphone is not supported.");
+//        }
+//
+//        TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(targetDataLineInfo);
+//        targetDataLine.open(audioFormat);
+//        targetDataLine.start();
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//
+//
+//        int bufferSize = 4096;
+//        byte[] buffer = new byte[bufferSize];
+//
+//        long startTime = System.currentTimeMillis();
+//
+//        System.out.println("Recording started.");
+//
+//        while (System.currentTimeMillis() - startTime < 5000) { // Adjust the duration of recording as needed
+//            int bytesRead = targetDataLine.read(buffer, 0, buffer.length);
+//            byteArrayOutputStream.write(buffer, 0, bytesRead);
+//        }
+//
+//        System.out.println("Recording finished.");
+//
+//        targetDataLine.stop();
+//        targetDataLine.close();
+//
+//        byte[] audioData = byteArrayOutputStream.toByteArray();
+//
+//
+//
+//
+//
+//        //Create Directory
+//        File directory = new File(fileDir);
+//        if (!directory.exists()) {
+//            boolean created = directory.mkdirs();
+//            if (!created) {
+//                throw new IllegalStateException("Failed to create the directory: " + fileDir);
+//            }
+//        }
+//        String name = file.getOriginalFilename();
+//
+//        Recordings recordings = new Recordings();
+//
+//        recordings.setName(name);
+//        recordings.setFormat(audioFormat.toString());
+//        recordings.setDuration(5000L);
+//        recordings.setBytes(audioData);
+//        recordings.setFilePath(fileDir + File.separator + name);
+//
+//
+//
+//        recordingRepository.save(recordings);
+//        return recordings;
+//
+//
+//    }
+//
+//    public Optional<Recordings> getRecordingById(String id) {
+//        return recordingRepository.findById(id);
+//    }
+//
+//    public void deleteRecordingById( String id) {
+//        recordingRepository.deleteById(id);
+//
+//    }
+//
+//    public byte[] getRecordingBytes(Recordings recordings) throws  Exception {
+//        return  recordings.getBytes();
+//    }
+//
+//
+//}
